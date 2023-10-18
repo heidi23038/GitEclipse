1227d5bfef32e8a5e004e6a0ac512564cc6ea16f
heidi23038 <heidi23038@student.hamk.fi>
2023-10-18 11:19:24 +0300

    Lisätty muuttujat ja ensimmäinen kysymys.

--- src/giteclipse.java
+++ src/giteclipse.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class giteclipse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+
+		Scanner in = new Scanner (System.in); 
+		String oikeaNimi = "Heidi";
+		String vastaus;
+		int arvaustenMaara = 0;
+		
+		System.out.println("Arvaa nimi?");
 	}
+	
 
 }
